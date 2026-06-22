14e74af4c3fd3d245082c7904021d8a7e07de694
minjko <komj36@gmail.com>
2026-06-22 17:36:11 +0900

    feat: Exception 처리

--- src/main/java/com/digniche/muntum/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/digniche/muntum/global/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.digniche.muntum.global.exception;
 
 import com.digniche.muntum.global.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * 전반적인 예외 처리
+ * Controller / Service에서의 전반적인 예외 처리
  */
 @Slf4j
 @RestControllerAdvice
