cee1e1e5384c5a6537958d0f721509ecbab6ed2e
minjko <komj36@gmail.com>
2026-06-28 19:14:04 +0900

    feat: 키워드 타입 한글 설명 추가

--- src/main/java/com/digniche/muntum/keyword/entity/KeywordType.java
+++ src/main/java/com/digniche/muntum/keyword/entity/KeywordType.java
@@ -1,7 +1,17 @@
 package com.digniche.muntum.keyword.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 키워드 유형(대분류)
+ */
+@Getter
+@RequiredArgsConstructor
 public enum KeywordType {
-    THEME,
-    SITUATION,
-    SUBJECT
+    THEME("테마"),
+    SUBJECTSITUATION("주제"),
+    SITUATION("상황");
+
+    private final String type_kr;
 }
