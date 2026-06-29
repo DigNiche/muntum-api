a798a5ffa3ebdded7fbfc9325b8595d4c57d82ec
minjko <komj36@gmail.com>
2026-06-29 15:56:09 +0900

    feat: 카테고리 컨버터 삭제

--- src/main/java/com/digniche/muntum/keyword/entity/KeywordCategory.java
+++ src/main/java/com/digniche/muntum/keyword/entity/KeywordCategory.java
@@ -3,6 +3,10 @@ package com.digniche.muntum.keyword.entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Getter
 @RequiredArgsConstructor
 public enum KeywordCategory {
@@ -16,4 +20,18 @@ public enum KeywordCategory {
     SITUATION("상황");
 
     private final String description;
+
+    // 키워드 등록 시, Literal 카테고리를 DISPLAY_CHAR 등을 이용하여 DB에 삽입 및 Display
+    public static String validateCategories(String rawCategories, String split_char, String concation_char) {
+
+
+        if (rawCategories == null || rawCategories.isBlank()) return "";
+        Set<String> validCategoryDescription = Arrays.stream(values())
+                .map(KeywordCategory::getDescription)
+                .collect(Collectors.toSet());
+        return Arrays.stream(rawCategories.split(split_char))
+                .map(String::trim)
+                .filter(validCategoryDescription::contains)
+                .collect(Collectors.joining(concation_char));
+    }
 }
