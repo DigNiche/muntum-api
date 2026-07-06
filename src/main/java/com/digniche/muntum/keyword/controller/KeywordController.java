c647e5c69d53c2fa900caebf1ad53fa9961ebed8
minjko <komj36@gmail.com>
2026-07-06 13:29:30 +0900

    feat: 프로그램과 연결된 키워드 목록만 조회

--- src/main/java/com/digniche/muntum/keyword/controller/KeywordController.java
+++ src/main/java/com/digniche/muntum/keyword/controller/KeywordController.java
@@ -5,7 +5,9 @@ import com.digniche.muntum.global.security.UserPrincipal;
 import com.digniche.muntum.keyword.dto.KeywordResponse;
 import com.digniche.muntum.keyword.dto.SelectKeywordsRequest;
 import com.digniche.muntum.keyword.dto.SelectedKeywordsResponse;
+import com.digniche.muntum.keyword.entity.Keyword;
 import com.digniche.muntum.keyword.service.KeywordService;
+import com.digniche.muntum.program.service.ProgramKeywordService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +26,7 @@ import java.util.List;
 public class KeywordController {
 
     private final KeywordService keywordService;
+    private final ProgramKeywordService programKeywordService;
 
     // 키워드 설정
     @PostMapping("/me/keyword")
@@ -40,6 +43,14 @@ public class KeywordController {
             @AuthenticationPrincipal UserPrincipal userPrincipal) {
         List<KeywordResponse> keywords = keywordService.retrieveSelectedKeywords(userPrincipal.getUserId());
         return ResponseEntity.ok(ApiResponse.success("내 취향 키워드 목록을 조회했습니다.", new SelectedKeywordsResponse(keywords)));
+    }
+
+    // 프로그램과 연결된 Keyword 목록 조회
+    @GetMapping("/keywords")
+    public ResponseEntity<ApiResponse<List<KeywordResponse>>> getProgramsKeywords() {
+        List<Keyword> keywords = programKeywordService.retrieveProgramsKeywords();
+        List<KeywordResponse> res = keywords.stream().map(KeywordResponse::from).toList();
+        return ResponseEntity.ok(ApiResponse.success("모든 프로그램의 키워드 목록을 조회했습니다.", res));
     }
 
 
